6c911942844eff260575a53070082158d9952359
mdrokz <mohammadmunshi@gmail.com>
2022-05-01 01:29:10 +0530

    build sass & link it to html

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 elm make src/Main.elm && mv index.html ./public/index.html
+sed '6i<link rel="stylesheet" href="./css/index.css"></link>' ./public/index.html > ./public/index_new.html
+rm ./public/index.html && mv ./public/index_new.html ./public/index.html
